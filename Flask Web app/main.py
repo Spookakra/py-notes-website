e4baf6ebc08c0ed78323d8ebddbe38c167e348f6
Spookakra <62347712+Spookakra@users.noreply.github.com>
2021-12-08 10:14:23 -0500

    created files

---
+++ Flask Web app/main.py
@@ -0,0 +1,8 @@
+from website import create_app
+
+app = create_app()
+
+#will only work if you run the file and not if you import it 
+if __name__ == '__main__':
+    #runs the webserver if you run the file 
+    app.run(debug=True)
